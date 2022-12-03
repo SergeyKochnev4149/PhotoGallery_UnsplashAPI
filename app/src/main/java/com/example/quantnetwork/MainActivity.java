e96bfdffea2898065e85d3c43bbdb301637bc7d1
Serhii Kochniev <sergeykochnev4149@gmail.com>
2022-12-04 01:21:45 +0200

    Requirements Program should read the content from site by API in JSON format
    Knowledge about this
    I didn't know how to do it. I found all the information on Google and YouTube
    
    1.Added Fast Android Networking Library.
    Problem:
    Caused by: org.gradle.internal.resolve.ModuleVersionNotFoundException: Could not find com.amitshekhar.android:android-networking:1.0.2.
    Solution: add "jcenter()" to gradle.properties ->dependencyResolutionManagement
    ->repositories
    
    I find solution on stackoverflow.com
    
    2.Added reading JSONObject from URL and getting username from that JSONObject

--- app/src/main/java/com/example/quantnetwork/MainActivity.java
+++ app/src/main/java/com/example/quantnetwork/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.quantnetwork;
 
 import android.content.Intent;
-import android.graphics.drawable.AdaptiveIconDrawable;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+
 
 public class MainActivity extends AppCompatActivity {
     static Drawable currentImage;
@@ -17,6 +32,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String url = "https://api.unsplash.com//photos//?client_id=ab3411e4ac868c2646c0ed488dfd919ef612b04c264f3374c97fff98ed253dc9";
+
+        AndroidNetworking.initialize(this);
+        AndroidNetworking.get(url)
+                .setPriority(Priority.HIGH)
+                .build()
+                .getAsJSONArray(new JSONArrayRequestListener() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("RES", response.toString());
+                        for (int i = 0; i < response.length(); i++) {
+                            String author = "author" + i;
+                            TextView currentView = findViewById(R.id.author1);
+
+
+                            try {
+                                JSONObject allImageInfo = response.getJSONObject(i);
+                                String authorUserName = allImageInfo.getJSONObject("user").getString("username");
+//                                JSONObject linksToImage = allImageInfo.getJSONObject("urls");
+//                                String imageAuthor = linksToImage.getString("small");
+
+//                                URL url = new URL("YourUrl");
+//                                Bitmap bitmap = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+
+                                currentView.setText(authorUserName);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        anError.printStackTrace();
+                        Log.e("ERROR", anError.toString());
+                    }
+                });
     }
 
     public void openPicture(View view) {
@@ -27,5 +81,8 @@ public class MainActivity extends AppCompatActivity {
 
         startActivity(intent);
     }
+
+//    private void create
+
 
 }
